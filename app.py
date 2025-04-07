dff5ee55abbf2b904d4d51252a4623e9662eccfb
slvr611 <orion.gesslein@gmail.com>
2025-04-07 02:13:18 -0400

    Wonder costs

--- app.py
+++ app.py
@@ -214,6 +214,35 @@ def change_list():
         preview_references=preview_overall_lookup_dict
     )
 
+@app.route("/wonders")
+def wonder_list():
+    schema, db = get_data_on_category("wonders")
+    
+    query_dict = {"_id": 1, "name": 1}
+    preview_overall_lookup_dict = {}
+    
+    for preview_item in schema.get("preview", {}):
+        query_dict[preview_item] = 1
+        collection_name = schema.get("properties", {}).get(preview_item, {}).get("collection", None)
+        if not collection_name is None:
+            preview_db = category_data[collection_name]["database"]
+            preview_individual_lookup_dict = {}
+            preview_data = list(preview_db.find({}, {"_id": 1, "name": 1}))
+            for data in preview_data:
+                preview_individual_lookup_dict[str(data["_id"])] = {"name": data.get("name", "None"), "link": collection_name + "/item/" + data.get("name", data.get("_id", "#"))}
+            preview_overall_lookup_dict[preview_item] = preview_individual_lookup_dict
+    
+    items = list(db.find({}, query_dict).sort("name", ASCENDING))
+    
+    return render_template(
+        "wonder_list.html",
+        title=category_data[data_type]["pluralName"],
+        items=items,
+        schema=schema,
+        preview_references=preview_overall_lookup_dict
+    )
+
+
 @app.route("/<data_type>")
 def data_list(data_type):
     schema, db = get_data_on_category(data_type)
