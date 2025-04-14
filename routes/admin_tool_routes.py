ba52a3bb0877a9c57d07f79a57b4086a73b74baf
slvr611 <orion.gesslein@gmail.com>
2025-04-14 00:58:45 -0400

    Added elected succession generator

--- routes/admin_tool_routes.py
+++ routes/admin_tool_routes.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, redirect
 from helpers.auth_helpers import admin_required
 from helpers.data_helpers import get_data_on_category, generate_id_to_name_dict, compute_demographics
 from calculations.field_calculations import calculate_all_fields
-from app_core import category_data, rarity_rankings, mongo
+from app_core import category_data, rarity_rankings, mongo, json_data
 from pymongo import ASCENDING
 import random
 
@@ -31,6 +31,52 @@ def demographics_overview():
         })
 
     return render_template("demographics_overview.html", demographics_list=demographics_list)
+
+@admin_tool_routes.route("/elected_candidates_generator")
+@admin_required
+def elected_candidates_generator():
+    stats_list = ["Rulership", "Cunning", "Charisma", "Prowess", "Magic", "Strategy"]
+
+    candidate_1_stats_list = stats_list.copy()
+
+    candidate_1_strengths = [candidate_1_stats_list.pop(random.randint(0, len(candidate_1_stats_list) - 1))]
+    candidate_1_strengths.append(candidate_1_stats_list.pop(random.randint(0, len(candidate_1_stats_list) - 1)))
+    candidate_1_weaknesses = [candidate_1_stats_list.pop(random.randint(0, len(candidate_1_stats_list) - 1))]
+    candidate_1_weaknesses.append(candidate_1_stats_list.pop(random.randint(0, len(candidate_1_stats_list) - 1)))
+
+    candidate_2_strengths = []
+    candidate_2_weaknesses = []
+
+    candidate_2_stats_list = stats_list.copy()
+    candidate_2_strengths = [candidate_2_stats_list.pop(random.randint(0, len(candidate_2_stats_list) - 1))]
+    candidate_2_strengths.append(candidate_2_stats_list.pop(random.randint(0, len(candidate_2_stats_list) - 1)))
+
+    while candidate_2_strengths == candidate_1_strengths:
+        candidate_2_stats_list = stats_list.copy()
+        candidate_2_strengths = [candidate_2_stats_list.pop(random.randint(0, len(candidate_2_stats_list) - 1))]
+        candidate_2_strengths.append(candidate_2_stats_list.pop(random.randint(0, len(candidate_2_stats_list) - 1)))
+    
+    candidate_2_stats_list_backup = candidate_2_stats_list.copy()
+    candidate_2_weaknesses = [candidate_2_stats_list_backup.pop(random.randint(0, len(candidate_2_stats_list_backup) - 1))]
+    candidate_2_weaknesses.append(candidate_2_stats_list_backup.pop(random.randint(0, len(candidate_2_stats_list_backup) - 1)))
+
+    while candidate_2_weaknesses == candidate_1_weaknesses:
+        candidate_2_stats_list_backup = candidate_2_stats_list.copy()
+        candidate_2_weaknesses = [candidate_2_stats_list_backup.pop(random.randint(0, len(candidate_2_stats_list_backup) - 1))]
+        candidate_2_weaknesses.append(candidate_2_stats_list_backup.pop(random.randint(0, len(candidate_2_stats_list_backup) - 1)))
+    
+    tier_1_positive_titles = []
+
+    for title_key, title_details in json_data["titles"].items():
+        if title_details["tier"] == 1 and title_details["type"] == "positive":
+            tier_1_positive_titles.append(title_details["display_name"])
+
+    candidate_1_title = random.choice(tier_1_positive_titles)
+    candidate_2_title = random.choice(tier_1_positive_titles)
+    while candidate_1_title == candidate_2_title:
+        candidate_2_title = random.choice(tier_1_positive_titles)
+
+    return render_template("elected_candidates_generator.html", candidate_1_strengths=candidate_1_strengths, candidate_1_weaknesses=candidate_1_weaknesses, candidate_2_strengths=candidate_2_strengths, candidate_2_weaknesses=candidate_2_weaknesses, candidate_1_title=candidate_1_title, candidate_2_title=candidate_2_title)
 
 @admin_tool_routes.route("/karma_helper")
 @admin_required
