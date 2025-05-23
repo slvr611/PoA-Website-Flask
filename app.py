beac0ef3d21eda2f0238416163a1ca9c1f35d6c2
slvr611 <orion.gesslein@gmail.com>
2025-05-22 23:29:20 -0400

    Forcing https

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 import os
 from app_core import app, mongo, discord, category_data, json_data
 from routes import register_routes
+from flask import request, redirect
 
 register_routes(app, mongo, discord)
+
+@app.before_request
+def before_request():
+    if not request.is_secure and not app.debug:
+        url = request.url.replace("http://", "https://", 1)
+        return redirect(url, code=301)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
